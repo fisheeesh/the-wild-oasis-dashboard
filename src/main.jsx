6da8639ccc24cd5f7f7f08d0bed13b27e06bcd29
fisheeesh <swanphyo444@gmail.com>
2025-03-28 16:22:44 +0700

    chore: display notification after actions

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import Router from './router/index.jsx'
 import GlobalStyles from './styles/GlobalStyles.js'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,5 +25,21 @@ createRoot(document.getElementById('root')).render(
     <ReactQueryDevtools initialIsOpen={false} />
     <GlobalStyles />
     <Router />
+
+    <Toaster position='top-center' gutter={12} containerStyle={{ margin: '8px', }} toastOptions={{
+      success: {
+        duration: 3000
+      },
+      error: {
+        duration: 5000
+      },
+      style: {
+        fontSize: '16px',
+        maxWidth: '500px',
+        padding: '16px 24px',
+        backgroundColor: 'var(--color-grey-0)',
+        color: 'var(--color-grey-700)'
+      }
+    }} />
   </QueryClientProvider>
 )
