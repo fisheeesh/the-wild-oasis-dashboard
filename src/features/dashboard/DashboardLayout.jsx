5737daf151a6b6d42ef07fb8a994b2e9ae899111
fisheeesh <swanphyo444@gmail.com>
2025-04-08 00:00:28 +0700

    feat: made dashbaord page layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,12 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistic</div>
+      <div>Todays Activity</div>
+    </StyledDashboardLayout>
+  )
+}
