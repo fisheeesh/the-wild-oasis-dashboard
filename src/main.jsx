b6848992abfa37fc32fd38eb94d35b73760ea662
fisheeesh <swanphyo444@gmail.com>
2025-04-09 01:11:54 +0700

    fix: conflict on live

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Router from './router/index.jsx'
 import GlobalStyles from './styles/GlobalStyles.js'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
