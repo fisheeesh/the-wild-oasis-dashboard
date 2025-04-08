4567c7f20e973611772f2c8a1106b6b7e9dad6c8
fisheeesh <swanphyo444@gmail.com>
2025-04-09 01:30:18 +0700

    fix: re-write supabase url thats ok i enabled rls

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+export const supabaseUrl = 'https://kqcnbskzonslscatafxy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxY25ic2t6b25zbHNjYXRhZnh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwODY0NDUsImV4cCI6MjA1ODY2MjQ0NX0.Ql8JQPkd7vcsAsmFod0MaiVlOQcLVY_GHwDncF-nZKw'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
